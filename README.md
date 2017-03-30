b36192e3b9e4648cb02de57144d6336a530edfd0
Oliver Lindemann <oliver.lindemann@cognitive-psychology.eu>
2017-03-30 10:34:25 +0200

    bash script

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Data Integrity Fingerprint (DIF)
 ================================
 
 *Released under the MIT License*
+
 Oliver Lindemann (oliver@expyriment.org) & Florian Krause (florian@expyriment.org)
 
 
--- bash/dif.sh
+++ bash/dif.sh
@@ -1,1 +1,28 @@
-todo
+#!/bin/bash
+# Produce Data Integrity Fingerprint
+#
+# Usage:
+#	dif.sh [-l] <DATA PATH>
+#
+# -p: print hash list
+#
+# O. Lindemann & F. Krause
+
+if [ "$1" = "-p" ]; then
+	find $2 -type f -print0 | xargs -0 sha256sum
+else
+	# find all files
+	# make hash list
+	# select only hashes
+	# sort them
+	# remove newlines
+	# create master hash
+	# print out only the master hash
+	find $1 -type f -print0 \
+			| xargs -0 sha256sum \
+			| cut -d' ' -f1 \
+			| sort \
+			| tr -d '\n' \
+			| sha256sum \
+			| cut -d' ' -f1
+fi
