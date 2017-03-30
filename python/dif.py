4d718a40ff6b38d248c907e7c21aac7f672fc5de
Florian Krause <florian.krause@fladd.de>
2017-03-30 19:10:44 +0200

    refactoring

--- python/dif.py
+++ python/dif.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-from __future__ import absolute_import,  unicode_literals, print_function
-
 import os
 import sys
+import codecs
 import hashlib
 import collections
 import multiprocessing
 from functools import partial
 
-PY3 = sys.version_info >= (3,0,0)
 
 class DataIntegrityFingerprint:
     """A class representing a DataIntegrityFingerprint (DIF).
@@ -21,13 +19,15 @@ class DataIntegrityFingerprint:
 
     """
 
-    def __init__(self, data, hash_algorithm="sha256"): # TODO: Why not specifying data folder with generate?
+    def __init__(self, data, from_checksums_file=False, hash_algorithm="sha256"):
         """Create a DataIntegrityFingerprint object.
 
         Parameters
         ----------
         data : str
             the path to the data
+        from_checksums_file : bool
+            data argument is a checksums file
         hash_algorithm : str
             the hash algorithm (optional, default: sha256)
 
@@ -36,20 +36,32 @@ class DataIntegrityFingerprint:
         if hash_algorithm not in hashlib.algorithms_guaranteed:
             raise ValueError("Hash algorithm '{0}' not supported.".format(
                 hash_algorithm))
-
         self._hash_algorithm = hash_algorithm
-        self._data = os.path.abspath(data)
         self._file_hashes = collections.OrderedDict()
-        if os.path.isfile(data):
-            self._file_hashes[data] = None
+        if from_checksums_file:
+            self._file_hashes = collections.OrderedDict()
+            with codecs.open(data, encoding="utf-8") as f:
+                for line in f:
+                    length = hashlib.new(self._hash_algorithm).digestsize * 2
+                    hash_ = line[:length]
+                    dir_ = line[length:].lstrip()
+                    self._data = dir_.split(os.path.sep)[0]
+                    root = os.path.split(self._data)[-1] + os.path.sep
+                    self._file_hashes[dir_.replace(root, "")] = hash_
+            self._generated = True
         else:
-            for dir_, _, files in os.walk(self._data):
-                for filename in files:
-                    filename = os.path.join(os.path.relpath(dir_, self._data),
-                                            filename)
-                    if filename.startswith("./"):
-                        filename = filename[2:]
-                    self._file_hashes[filename] = None
+            self._data = os.path.abspath(data)
+            if os.path.isfile(data):
+                self._file_hashes[data] = None
+            else:
+                for dir_, _, files in os.walk(self._data):
+                    for filename in files:
+                        filename = os.path.join(os.path.relpath(dir_, self._data),
+                                                filename)
+                        if filename.startswith("./"):
+                            filename = filename[2:]
+                        self._file_hashes[filename] = None
+            self._generated = False
 
     def __str__(self):
         return str(self._master_hash)
@@ -64,11 +76,8 @@ class DataIntegrityFingerprint:
 
     @property
     def _master_hash(self):
-        """DOCU"""
-
-        if self._file_hashes is None:
+        if not self._generated:
             return None
-
         file_hashes = sorted(self._file_hashes.values())
         if len(file_hashes) > 1:
             hasher = hashlib.new(self._hash_algorithm)
@@ -100,32 +109,14 @@ class DataIntegrityFingerprint:
                     multiprocessing.Pool().imap_unordered(func, tmp)):
                 if progress is not None:
                     progress(counter + 1, len(self._file_hashes),
-                             "{0}/{1} files".format(counter + 1,
-                                                    len(self._file_hashes)))
+                             "{0}/{1}".format(counter + 1,
+                                              len(self._file_hashes)))
                 self._file_hashes[rtn[0].replace(
                     self._data + os.path.sep, "")] = rtn[1]
         else:
             rtn = _hash_file(self._file_hashes.keys()[0])
             self._file_hashes[rtn[0]] = rtn[1]
-
-
-    def load_hash_file(self, filename):
-        """load hash list"""
-        self._file_hashes = {}
-        with open(filename, "rb") as fl:
-            for l in fl:
-                tmp = string_decode(l).strip().split("  ")
-                if len(tmp) == 2:
-                    self._file_hashes[tmp[1]] = tmp[0]
-
-
-def string_decode(s, enc='utf-8', errors='strict'):
-    """bytestring to unicode"""
-
-    if (PY3 and isinstance(s, str)) or (not PY3 and isinstance(s, unicode)):
-        return s
-    if (PY3 and isinstance(s, bytes)) or (not PY3 and isinstance(s, str)):
-        return s.decode(enc, errors)
+        self._generated = True
 
 
 def _hash_file(filename, hash_algorithm):
@@ -144,13 +135,13 @@ if __name__ == "__main__":
             epilog="(c) F. Krause & O. Lindemann")
     parser.add_argument("PATH", nargs='?', default=None,
                         help="the path to the data folder or file")
-    parser.add_argument("-c", "--checksums", dest="checksums",
+    parser.add_argument("-s", "--save-checksums-file", dest="savechecksumsfile",
                         action="store_true",
-                        help="save checksums to file",
+                        help="save checksums file",
                         default = False)
-    parser.add_argument("-f", "--hashfile", dest="hashfile",
+    parser.add_argument("-f", "--from-checksums-file", dest="fromchecksumsfile",
                         action="store_true",
-                        help="create DIF from existing hash file",
+                        help="PATH is a checksums file",
                         default = False)
     args = vars(parser.parse_args())
 
@@ -170,17 +161,15 @@ if __name__ == "__main__":
         sys.stdout.write('{:5.1f}% [{}] {}\r'.format(percents, bar, status))
         sys.stdout.flush()
 
-    if args['hashfile']:
-        dif = DataIntegrityFingerprint(data=".", hash_algorithm="sha256")
-        dif.load_hash_file(args["PATH"])
+    if args['fromchecksumsfile']:
+        dif = DataIntegrityFingerprint(args["PATH"], True, "sha256")
     else:
-        dif = DataIntegrityFingerprint(data=args["PATH"], hash_algorithm="sha256")
+        dif = DataIntegrityFingerprint(args["PATH"], False, "sha256")
         dif.generate(progress=progress)
         print("")
     print("DIF: {0}".format(dif))
 
-    if args['checksums']:
-        import codecs
+    if args['savechecksumsfile']:
         outfile = os.path.split(dif.data)[-1] + ".sha256"
         answer = "y"
         if os.path.exists(outfile):
@@ -190,7 +179,7 @@ if __name__ == "__main__":
             with codecs.open(outfile, 'w', encoding="utf-8") as f:
                 for filename in dif.file_hashes:
                     f.write(dif.file_hashes[filename] + "  " + os.path.join(
-                        os.path.split(dif._data)[1], filename + "\n"))
+                        os.path.split(dif._data)[-1], filename + "\n"))
             print("Checksums have been written to '{0}'.".format(outfile))
         else:
             print("Checksums have NOT been written.".format(outfile))
