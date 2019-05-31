782a0748e632e08b066d04c197c291368abfa288
Oliver Lindemann <lindemann09@users.noreply.github.com>
2019-05-31 13:23:37 +0200

    Update dif.sh

--- bash/dif.sh
+++ bash/dif.sh
@@ -20,9 +20,7 @@ else
 	# print out only the master hash
 	find $1 -type f -print0 \
 			| xargs -0 sha256sum \
-			| cut -d' ' -f1 \
 			| sort \
-			| tr -d '\n' \
 			| sha256sum \
 			| cut -d' ' -f1
 fi
